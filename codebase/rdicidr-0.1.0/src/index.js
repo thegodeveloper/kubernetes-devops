628eee84132aa85e273bdb7fef9f959247b64c49
William Munoz <william@godeveloper.io>
2024-12-28 23:36:47 -0500

    first ci test

--- codebase/rdicidr-0.1.0/src/index.js
+++ codebase/rdicidr-0.1.0/src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// testing ci
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
