3feed62f00f7c6c111b7fd7843ff73f7c0c955fb
William Munoz <william@godeveloper.io>
2024-12-28 23:41:03 -0500

    test should work

--- codebase/rdicidr-0.1.0/src/App.test.js
+++ codebase/rdicidr-0.1.0/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/CIDR Calculator777/i);
+  const linkElement = screen.getByText(/CIDR Calculator/i);
   expect(linkElement).toBeInTheDocument();
 });
